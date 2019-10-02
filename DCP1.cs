6277b22b9bdfbd87efa41f288c19443c8a339f68
Hayden Migliore <53566995+hayden-migliore@users.noreply.github.com>
2019-10-02 15:05:52 -0500

    Add files via upload

---
+++ DCP1.cs
@@ -0,0 +1,60 @@
+﻿// Author: Hayden Migliore
+// Date: August 1, 2019
+// Problem: Given a list of numbers and a number k, 
+//      return whether any two numbers form the list 
+//      add up to k. For example, given [10, 15, 3, 7] 
+//      and k of 17, return true since 10 + 7 is 17. 
+// Bonus: Can you do this in one pass? 
+
+using System;
+
+namespace DCP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intialize variables
+            Boolean addition = false;
+            int[] set = new int[5];
+
+            //Create Set through Console
+            Console.WriteLine("Please enter 5 numbers for set.");
+            set[0] = Convert.ToInt32(Console.ReadLine());
+            set[1] = Convert.ToInt32(Console.ReadLine());
+            set[2] = Convert.ToInt32(Console.ReadLine());
+            set[3] = Convert.ToInt32(Console.ReadLine());
+            set[4] = Convert.ToInt32(Console.ReadLine());
+
+            //Create k through Console
+            Console.WriteLine("Please enter K.");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            //Test set and K
+            if (set[0] + set[1] == k)
+                addition = true;
+            else if (set[0] + set[2] == k)
+                addition = true;
+            else if (set[0] + set[3] == k)
+                addition = true;
+            else if (set[0] + set[4] == k)
+                addition = true;
+            else if (set[1] + set[2] == k)
+                addition = true;
+            else if (set[1] + set[3] == k)
+                addition = true;
+            else if (set[1] + set[4] == k)
+                addition = true;
+            else if (set[2] + set[3] == k)
+                addition = true;
+            else if (set[2] + set[4] == k)
+                addition = true;
+            else if (set[3] + set[4] == k)
+                addition = true;
+
+            //Output
+            Console.WriteLine("Output is: " + addition);
+        }
+    }
+}
+
---
+++ DCP2.cs
@@ -0,0 +1,55 @@
+﻿// Author: Hayden Migliore
+// Date: August 5, 2019
+// Problem: Given an array of integers, return an new
+//      array such that each element at index i of the
+//      new array is the product of all of the numbers
+//      in the original array except the one at i. For
+//      exampe if our input was [1, 2, 3, 4, 5] the
+//      output woudl be [120, 60, 40, 30, 24]. If our
+//      input was [3, 2, 1], the expected output would
+//      be [2, 3, 6]. Follow-up: waht if you can't use
+//      division?
+
+using System;
+
+namespace DCP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intialize variables
+            Console.WriteLine("Set size of array.");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            int[] set = new int[i];
+            int[] answer = new int[i];
+
+            //Create Set through Console
+            Console.WriteLine("Please enter " + i + " numbers for set.");
+            for (int j = 0; j < i; j++)
+            {
+                set[j] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Process new set
+            for (int j = 0; j < i; j++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    if (k != j)
+                        sum = sum * set[k];
+                }
+                answer[j] = sum;
+                sum = 1;
+            }
+
+            //Output new set
+            Console.WriteLine("The ouput is ");
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(answer[j] + " ");
+            }
+        }
+    }
+}
